528933a7319fd58da83093df2a3b8a5215def2a8
Erik MacLennan <erikmaclennan@alumni.ubc.ca>
2016-07-16 21:55:14 -0700

    connecting all includes sensibly, adding function signatures.

--- TheRay/Robot/Actuators.hpp
+++ TheRay/Robot/Actuators.hpp
@@ -9,6 +9,32 @@
 #ifndef Actuators_hpp
 #define Actuators_hpp
 
-#include <stdio.h>
+#ifndef phys253_h
+#define phys253_h
+#include "phys253.h"
+#endif
+
+#ifndef Config_hpp
+#define Config_hpp
+#include "Config.hpp"
+#endif
+
+void turnInPlace(int turnAngle);
+void drive(Velocity velocity, bool turnRight, Turn turn, bool reverse = false);
+
+enum Velocity {
+    Stop = 0,
+    Slow = VELOCITY_SLOW,
+    Normal = VELOCITY_NORMAL,
+    Fast = VELOCITY_FAST
+};
+
+enum Turn {
+    Straight = 0,
+    SlightRight = 0,
+    SlightLeft = 0,
+    HardRight = 0
+};
+
 
 #endif /* Actuators_hpp */
