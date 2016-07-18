6de9ef9f6a2c816fe2fa1f5ad24a437844c200f4
Morten Kals <m.kals@me.com>
2016-07-18 12:22:02 -0700

    Updated collision sensor readings

--- TheRay/Collision.cpp
+++ TheRay/Collision.cpp
@@ -8,18 +8,28 @@
 #include "Collision.hpp"
 
 
-
-Collision::Result Collision::check() {
+Collision::Result checkSensors() {
+    bool left = digitalRead(TOUCH_SWITCH_LEFT);
+    bool right = digitalRead(TOUCH_SWITCH_RIGHT);
+    bool back = digitalRead(TOUCH_SWITCH_BACK);
     
-    bool left = analogRead(TOUCH_SWITCH_LEFT);
-    bool right = analogRead(TOUCH_SWITCH_RIGHT);
-    bool back = analogRead(TOUCH_SWITCH_BACK);
-    
-    if (left && right) return Collision::Both;
-    else if (left) return Collision::Left;
-    else if (right) return Collision::Right;
+    if (!left && !right) return Collision::Both;
+    else if (!left) return Collision::Left;
+    else if (!right) return Collision::Right;
     else return Collision::None;
 }
 
-
-
+Collision::Result Collision::check() {
+    Collision::Result firstCheck = checkSensors();
+    
+    if (firstCheck == Collision::None) {
+        return firstCheck;
+    } else {
+        Collision::Result secondCheck = checkSensors();
+        if (secondCheck != Collision::None) {
+            return secondCheck;
+        }
+    }
+    return Collision::None;
+    
+}
